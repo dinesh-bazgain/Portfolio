b3ac6e7505ba9ef7d22bd0fdccf2441696010b5f
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-10-10 18:19:03 +0545

    feat: import Portfolio component in layout for enhanced structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Portfolio from "./Portfolio";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
