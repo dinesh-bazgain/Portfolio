2cce9e964a33bfbd9c4658a6aa856689a114804c
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-10-11 08:26:48 +0545

    feat: restructure Home component to include Navbar, Footer, and main content sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+import Link from "next/link";
+import Image from "next/image";
+import Footer from "../components/layouts/Footer";
+import Navbar from "../components/layouts/Navbar";
+import HomePage from "./home/page";
+import WorkWithMe from "./workWithMe/page";
+import ContactPage from "./contact/page";
+import ProjectListPage from "./project/page";
+
 export default function Home() {
   return (
-    <main>
-      <h1>this is me </h1>
-    </main>
+    <div className="flex flex-col min-h-screen bg-white">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Main Content */}
+      <section id="home">
+        <HomePage />
+      </section>
+      <section id="projects">
+        <ProjectListPage />
+      </section>
+      <section id="WorkWithMe">
+        <WorkWithMe />
+      </section>
+      <section id="contact">
+        <ContactPage />
+      </section>
+
+      {/* Footer */}
+      <Footer />
+    </div>
   );
 }
