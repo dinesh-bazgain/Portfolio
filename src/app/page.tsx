c85519e58e248716a3be70aeaeea2e9086ad8914
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-12-08 12:03:07 +0545

    refactor: Simplify Home component by removing unused sections and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,9 @@
-import Footer from "@/components/layouts/Footer";
-import Navbar from "@/components/layouts/Navbar";
 import HomePage from "./home/page";
-import WorkWithMe from "./workWithMe/page";
-import ContactPage from "./contact/page";
-import ProjectListPage from "./project/page";
 
 export default function Home() {
   return (
-    <div>
-      {/* Navbar */}
-      <Navbar />
-
-      {/* Main Content */}
-      <section id="home">
-        <HomePage />
-      </section>
-      <section id="projects">
-        <ProjectListPage />
-      </section>
-      <section id="WorkWithMe">
-        <WorkWithMe />
-      </section>
-      <section id="contact">
-        <ContactPage />
-      </section>
-
-      {/* Footer */}
-      <Footer />
-    </div>
+    <>
+      <HomePage />
+    </>
   );
 }
